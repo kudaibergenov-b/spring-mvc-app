ccd0cdae152a6cdea798aee68f9596eb9e5688cb
b_kudaibergenov <kudaibergenov_b@auca.kg>
2024-12-27 16:37:43 +0600

    init

--- src/main/java/com/kudaibergenov/spring/config/SpringConfig.java
+++ src/main/java/com/kudaibergenov/spring/config/SpringConfig.java
@@ -12,8 +12,6 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
-
-
 @Configuration
 @ComponentScan("com.kudaibergenov.spring")
 @EnableWebMvc
